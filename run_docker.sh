cdd4603e0d7df1658e2c54fa1260c257ade21ecd
irtaza06 <irtazahassan06@gmail.com>
2020-05-18 11:53:17 +0200

    Adding project files

---
+++ run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build . --tag='predictionapp:test'
+# Step 2: 
+# List docker images
+docker images --all
+# Step 3: 
+# Run flask app
+#Expose port 80 from the Docker container on port 5000 of my laptop
+docker run -p 5000:80 predictionapp:test
---
+++ run_kubernetes.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=irtaza06/predictionapp:test
+podname=kubernetes-predictionapp
+podpath=pods/$podname
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run $podname --image=$dockerpath
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+#Step 4
+# Descibe pod
+kubectl describe $podpath
+
+#waiting for deployment
+sleep 1m
+
+# Step 5:
+# Forward the container port to a host
+kubectl port-forward $podpath 5000:80
+
+echo "FINISHED!"
